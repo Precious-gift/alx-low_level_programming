d1c1ac79b88a5d212ec75f4f9cf99e10783ab76d
Precious-gift <falayeprecious@gmail.com>
2021-06-18 13:22:19 -0700

    pls

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-  char c;  
+char c;
 for (c = 'a'; c <= 'z'; c++)
-    { putchar(c); }
+{ putchar(c); }
 for (c = 'A'; c <= 'Z'; c++)
-    { putchar(c); }
+{ putchar(c); }
 putchar('\n');
 return (0);
 }
