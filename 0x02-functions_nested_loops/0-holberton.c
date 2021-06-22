9aef162583bf3f714236029989bf6cb4562cac86
Precious-gift <falayeprecious@gmail.com>
2021-06-22 04:59:32 -0700

    pls work

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - to print holberton.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+  int ch = 'Holberton';
+
+  _putchar(ch);
+
+  return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Functions
+
+Nested loops
